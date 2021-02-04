7bb9ac78e5aaeb8ec8dcbc0f9b1d53767f461191
Juno Yu <jxy35@hitit.xyz>
2021-02-04 14:42:35 +0900

    first

---
+++ src/Todo.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { fly, scale } from "svelte/transition";
+  export let todo;
+  export let remove;
+</script>
+
+<div
+  class="notification"
+  class:is-success={todo.completed}
+  transition:fly={{ duration: 1000, x: 50 }}>
+  <input type="checkbox" bind:checked={todo.completed} />
+  {todo.title}
+  <button class="delete" on:click={() => remove(todo)} />
+</div>
